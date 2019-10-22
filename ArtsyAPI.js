043f674f18b4c9064414d0e0338d48dca327c365
smirnovdv <volmondo@gmail.com>
2019-10-21 21:33:00 -0500

    home page completed in react

--- ArtsyAPI.js
+++ ArtsyAPI.js
@@ -1,21 +1,29 @@
-//this code shows first 20 artworks by Klimt
+//this code downloads links to HD artwork pics and other related data
 
-var traverson = require('traverson'),
-    JsonHalAdapter = require('traverson-hal'),
-    xappToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI1ZGEwYjlmMDYzN2E4YzAwMTJhZWY3M2EiLCJleHAiOjE1NzE3MDY0MzEsImlhdCI6MTU3MTEwMTYzMSwiYXVkIjoiNWRhMGI5ZjA2MzdhOGMwMDEyYWVmNzNhIiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjVkYTUxYmJmYWRhNTU0MDAwZDVjNmUwYiJ9.HBST5xH5xAmAjD3Zyme3mBoeVIoqcoo2uzhn_nhvORM';
-
+// we are using traverson and traverson-hal modules for HATEOAS (Hypermedia as the Engine of Application State) API of artsy
+const traverson = require('traverson');
+const    JsonHalAdapter = require('traverson-hal');
+//API key
+const    xappToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI1ZGEwYjlmMDYzN2E4YzAwMTJhZWY3M2EiLCJleHAiOjE1NzE3MDY0MzEsImlhdCI6MTU3MTEwMTYzMSwiYXVkIjoiNWRhMGI5ZjA2MzdhOGMwMDEyYWVmNzNhIiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjVkYTUxYmJmYWRhNTU0MDAwZDVjNmUwYiJ9.HBST5xH5xAmAjD3Zyme3mBoeVIoqcoo2uzhn_nhvORM';
 traverson.registerMediaType(JsonHalAdapter.mediaType, JsonHalAdapter);
+//endpoint
 api = traverson.from('https://api.artsy.net/api').jsonHal();
-
-api.newRequest()
-  .follow('artists')
+//making request for artworks wher artist has paticular artist_id, returning max of 100 artworks
+  api.newRequest()
+  .follow('artworks')
   .withRequestOptions({
     headers: {
       'X-Xapp-Token': xappToken,
       'Accept': 'application/vnd.artsy-v2+json'
     }
   })
- .withTemplateParameters({ sortable_name: "Warhol Andy" })
-  .getResource(function(error, andyWarhol) {
-    console.log(andyWarhol);
+ .withTemplateParameters({ artist_id: "4d8b92b64eb68a1b2c000414",size:100 })
+  .getResource(function(error, data) {
+    data._embedded.artworks.map((artwork)=>{
+      console.log(artwork.title);
+      console.log(artwork.title);
+      console.log(artwork._links.image.href.replace("{image_version}", "normalized"));
+    })
+    
+
   });
