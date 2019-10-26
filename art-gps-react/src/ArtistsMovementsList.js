7ce2e172d20dd06dee9994f7d1a3a8b98e24fc5e
smirnovdv <volmondo@gmail.com>
2019-10-25 19:56:24 -0500

    started with responsive

--- art-gps-react/src/ArtistsMovementsList.js
+++ art-gps-react/src/ArtistsMovementsList.js
@@ -10,31 +10,31 @@ function ArtistsMovementsList() {
             {/* Arrow divs underline text when line is hovered */}
             <ul>
                 <li>
-                    <span className="artist">Andy Warhol</span>.......<span className="movement">Pop<span className="colored">-</span>art</span>     
+                    <span className="artist">Andy Warhol</span>.......<span className="movement">Pop<span className="colored">-</span>art </span>     
                     <div className='upArrow'></div>
                     <div className='downArrow'></div>
                 </li>
                 <li>
-                    <span className="artist">Claude Monet</span>.......<span className="movement">Impress<span className="colored">i</span>on<span className="colored"></span>ism</span>
+                    <span className="artist">Claude Monet</span>.......<span className="movement">Impress<span className="colored">i</span>on<span className="colored"></span>ism </span>
                     <div className='upArrow'></div>
                     <div className='downArrow'></div>
                 </li>
                 <li>
-                    <span className="artist">Gustav Klimt</span>.......<span className="movement">Symbol<span className="colored">i</span>sm</span> 
+                    <span className="artist">Gustav Klimt</span>.......<span className="movement">Symbol<span className="colored">i</span>sm </span> 
                     <div className='upArrow'></div>
                     <div className='downArrow'></div>
                 </li>
                 <li>
-                    <span className="artist">Kazimir Malevich</span>.......<span className="movement">Futur<span className="colored">i</span>sm</span> 
+                    <span className="artist">Kazimir Malevich</span>.......<span className="movement">Futur<span className="colored">i</span>sm </span> 
                     <div className='upArrow'></div>   
                     <div className='downArrow'></div>
                 </li>
                 <li>
-                    <span className="artist">Pablo Picasso</span>.......<span className="movement">Cub<span className="colored">i</span>sm</span>
+                    <span className="artist">Pablo Picasso</span>.......<span className="movement">Cub<span className="colored">i</span>sm </span>
                     <div className='upArrow'></div>
                     <div className='downArrow'></div>
                 </li>
-                    <li><span className="artist">Salvador Dali</span>.......<span className="movement">Surreal<span className="colored">i</span>sm</span> 
+                    <li><span className="artist">Salvador Dali</span>.......<span className="movement">Surreal<span className="colored">i</span>sm </span> 
                     <div className='upArrow'></div>
                     <div className='downArrow'></div>
                 </li>
