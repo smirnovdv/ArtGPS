25c05cf1b92fc38de9dc442775a4fb835b91aab6
smirnovdv <volmondo@gmail.com>
2019-11-14 21:27:47 -0600

    answer clicks working

--- art-gps-react/src/ChallengePage.js
+++ art-gps-react/src/ChallengePage.js
@@ -1,51 +1,19 @@
 import React, {Component} from 'react';
 import './css/ChallengePage.css';
 import Navbar from './Navbar';
-import ChallengeScore from './ChallengeScore';
 import ChallengePic from './ChallengePic';
-import ChallengeQnA from './ChallengeQnA';
-import ChallengeRightAnswer from './ChallengeRightAnswer';
+
 
 
 
 class ChallengePage extends Component{
-    constructor(){
-        super();
-        this.state = {
-            artworks:[{image_url:""},{image_url:""},{image_url:""}],
-            score:0
-        }
-    }
-    
-    componentDidMount() {
-        //fetching "proxy": "http://localhost:3001",
-        fetch(`/get_challenge`)
-        .then(
-            (response)=> {
-            if (response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' +
-                response.status);
-                return;
-            }
-
-            // Examine the text in the response
-            response.text().then((artwork)=> {
-                console.log("Server response is" + JSON.parse(artwork));
-                this.setState({artworks:JSON.parse(artwork)})
-            });
-            }
-        )
-        .catch(function(err) {
-            console.log('Fetch Error :-S', err);
-        });
-    }
     
     render(){
         return (
             <div className="challengePage">
                 <Navbar activePage="challenge"/>
                 {/* <ChallengeScore/> */}
-                <ChallengePic artworks = {this.state.artworks}/>
+                <ChallengePic />
                 {/* <ChallengeQnA/>
                 <ChallengeRightAnswer/> */}
             </div>
--- art-gps-react/src/ChallengePic.js
+++ art-gps-react/src/ChallengePic.js
@@ -1,33 +1,83 @@
 import React, { Component } from 'react';
 import './css/ChallengePic.css'
 
-export default function ChallengePic(props) {
-    const image = props.artworks[0].image_url.replace("{image_version}","large");
-    let randomArray = [0,1,2];
+export default class ChallengePic extends Component {
+    constructor(){
+        super();
+        this.state = {
+            artworks:[{image_url:""},{image_url:""},{image_url:""}],
+            score:0,
+            time:0
+        }
+    }
+    fetchPic() {
+        fetch(`/get_challenge`)
+        .then(
+            (response)=> {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+                return;
+            }
+
+            // Examine the text in the response
+            response.text().then((artwork)=> {
+                let d = new Date();
+                console.log("Server response is" + JSON.parse(artwork));
+                this.setState({artworks:JSON.parse(artwork),
+                               time:d.getTime()}
+                )
+            });
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+    }
+    componentDidMount() {
+        //fetching "proxy": "http://localhost:3001",
+        this.fetchPic()
+    }
+    
+    
     // clicks on answers
-    const handleClick = (e) => {
-        if (e.target.innerText === props.artworks[0].title.trim()) {
-            e.target.classList="clickedRight";
-            setTimeout(()=>{setState})
+    handleClick = (e) => {
+        let target = e.target;
+        if (target.innerText === this.state.artworks[0].title.trim()) {
+            target.classList="clickedRight";
+            let d = new Date();
+            
+            setTimeout(()=>{
+                this.setState((prevState, props) => ({
+                    score: prevState.score  +Math.round(10000000/(d.getTime() - prevState.time))
+                }));
+                target.classList="answer";
+                this.fetchPic()},2000)
         }
         else {
-            e.target.classList="clickedWrong";
+            target.classList="clickedWrong";
+            setTimeout(()=>{
+                target.classList="answer"},1000)
         }
     }
     // const answer = <button className="answer">{props.artworks[randomArray.splice(Math.floor(Math.random()*3),1)].title}</button>
-    return (
-        <div className="Challenge">
-            <div className="challengePic" style={{backgroundImage:'url("'+image+'")'}}></div>
-            <div className="buttons"> 
-                <p className="challengeQuestion">What is the name of this artwork?</p>
-                {/* randomized buttons */}
-                <button className="answer" onClick={handleClick}>{props.artworks[randomArray.splice(Math.floor(Math.random()*3),1)].title}</button>
-                <button className="answer" onClick={handleClick}>{props.artworks[randomArray.splice(Math.floor(Math.random()*2),1)].title}</button>
-                <button className="answer" onClick={handleClick}>{props.artworks[randomArray.splice(Math.floor(Math.random()*1),1)].title}</button>
+    render(){
+        let image = this.state.artworks[0].image_url.replace("{image_version}","large");
+        let randomArray = [0,1,2];
+        return (
+            <div className="Challenge">
+                <div className="challengePic" style={{backgroundImage:'url("'+image+'")'}}></div>
+                <div className="buttons"> 
+                    <p className="challengeQuestion">What is the name of this artwork?</p>
+                    <p className="challengeQuestion">Your score: {this.state.score}</p>
+                    {/* randomized buttons */}
+                    <button className="answer" onClick={this.handleClick}>{this.state.artworks[randomArray.splice(Math.floor(Math.random()*3),1)].title}</button>
+                    <button className="answer" onClick={this.handleClick}>{this.state.artworks[randomArray.splice(Math.floor(Math.random()*2),1)].title}</button>
+                    <button className="answer" onClick={this.handleClick}>{this.state.artworks[randomArray.splice(Math.floor(Math.random()*1),1)].title}</button>
+                </div>
             </div>
-        </div>
-        
-    )
+        )
+    }
 }
 
 
