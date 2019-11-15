252850012e4e940eba8869f6a27084016e440ba9
smirnovdv <volmondo@gmail.com>
2019-11-14 20:35:34 -0600

    answer clicks working

--- art-gps-react/src/ChallengePic.js
+++ art-gps-react/src/ChallengePic.js
@@ -4,16 +4,26 @@ import './css/ChallengePic.css'
 export default function ChallengePic(props) {
     const image = props.artworks[0].image_url.replace("{image_version}","large");
     let randomArray = [0,1,2];
-    console.log(image)
+    // clicks on answers
+    const handleClick = (e) => {
+        if (e.target.innerText === props.artworks[0].title.trim()) {
+            e.target.classList="clickedRight";
+            setTimeout(()=>{setState})
+        }
+        else {
+            e.target.classList="clickedWrong";
+        }
+    }
     // const answer = <button className="answer">{props.artworks[randomArray.splice(Math.floor(Math.random()*3),1)].title}</button>
     return (
         <div className="Challenge">
             <div className="challengePic" style={{backgroundImage:'url("'+image+'")'}}></div>
-            
-            <div className="buttons">
-                <button className="answer">{props.artworks[randomArray.splice(Math.floor(Math.random()*3),1)].title}</button>
-                <button className="answer">{props.artworks[randomArray.splice(Math.floor(Math.random()*2),1)].title}</button>
-                <button className="answer">{props.artworks[randomArray.splice(Math.floor(Math.random()*1),1)].title}</button>
+            <div className="buttons"> 
+                <p className="challengeQuestion">What is the name of this artwork?</p>
+                {/* randomized buttons */}
+                <button className="answer" onClick={handleClick}>{props.artworks[randomArray.splice(Math.floor(Math.random()*3),1)].title}</button>
+                <button className="answer" onClick={handleClick}>{props.artworks[randomArray.splice(Math.floor(Math.random()*2),1)].title}</button>
+                <button className="answer" onClick={handleClick}>{props.artworks[randomArray.splice(Math.floor(Math.random()*1),1)].title}</button>
             </div>
         </div>
         
